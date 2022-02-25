f6479fb9e9b7c58b448f75cb39a51af77cab5d6c
ruanjunbin <ruanjunbin@bytedance.com>
2022-02-25 16:34:35 +0800

    Add s to order session by name

--- join_pane.sh
+++ join_pane.sh
@@ -5,6 +5,7 @@ srcWindowName=$(tmux display-message -t "$srcWindowIndex" -p "#W")
 currentWindowName=$(tmux display-message -p "#W")
 
 tmux join-pane -h -s "$srcWindowIndex" || exit 0
+# tmux join-pane -s "$srcWindowIndex" || exit 0
 # The index of left pane is 1.
 tmux select-pane -t 1
 tmux rename-window "$currentWindowName/$srcWindowName"
